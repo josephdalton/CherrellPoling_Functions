cf92ddfd4d0adda44063eb55f4428a3c82aea54b
cherrellpoling <cpoling@fullsail.edu>
2013-09-26 15:54:09 -0400

    stated the problem and how to fix it.

---
+++ Functions_Industry/js/script.js
@@ -0,0 +1,7 @@
+// Cherrell Poling 9/25/13 Functions Industry
+
+// going to use else if && at least one operator && Named Functions
+
+//In order to upgrade your phone, you must not have a past due balance, 
+//your commitment must be finished, and you have to be able a downpayment for the phone. 
+
